7ddd25eafd437193096629f3fb158f1a19f36b17
Sergey Shapovalov <scotty200305@gmail.com>
2024-01-07 23:40:53 +0400

    Второй коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+
     static Scanner scanner = new Scanner(System.in);
     static Calculator calculator = new Calculator();
     static Product product = new Product();
     static Form form = new Form();
     public static void main(String[] args) {
         System.out.println("На скольких человек необходимо разделить счёт?");
-        int numberOfPeople = scanner.nextInt();
+        int numberOfPeople = scanner.hasNextInt()? scanner.nextInt() : 0;
         while (numberOfPeople<2) {
-            System.out.println("Ошибка. Количество гостей должно быть больше одного. Введите коректное значение.");
-            numberOfPeople = scanner.nextInt();
+            System.out.println("Ошибка. Введите корректное значение.");
+            scanner.nextLine();
+            numberOfPeople = scanner.hasNextInt()? scanner.nextInt() : 0;
         }
         calculator.calculate(product, scanner, numberOfPeople);
         System.out.println("Добавленные товары:\n" + product.name + "\nКаждый должен заплатить по " + calculator.strResult + " " + form.ruble(calculator.dResult));
     }
-
-
-
-
 }
+
+
 class Product {
     String name = "";
     Double price = 0.0;
@@ -29,12 +29,19 @@ class Calculator {
     String strResult;
     Double dResult;
     void calculate(Product product, Scanner scanner, int numberOfPeople) {
-
+        double price;
         while (true) {
             System.out.println("Введите название товара.");
             product.name += scanner.next() + "\n";
             System.out.println("Введите стоимость товара.");
-            product.price += scanner.nextDouble();
+            scanner.nextLine();
+            price = scanner.hasNextDouble()? scanner.nextDouble() : -1.0;
+            while (price < 0) {
+                System.out.println("Ошибка. Введите корректное значение.");
+                scanner.nextLine();
+                price = scanner.hasNextDouble()? scanner.nextDouble() : -1.0;
+            }
+            product.price += price;
             System.out.println("Хотите добавить ещё один товар? Введите 'Завершить', если нет");
             if (scanner.next().equalsIgnoreCase("завершить")) {break;}
         }
